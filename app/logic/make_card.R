4612a5864e92ed97c8001e7e7d6bda1c75749273
davidrsch <daviddrsch@gmail.com>
2024-09-01 18:17:23 +0200

    Fixing style make card

--- app/logic/make_card.R
+++ app/logic/make_card.R
@@ -1,13 +1,13 @@
 box::use(
-  shiny.fluent[Stack, Text],
-  htmltools[div, tags],
   glue[glue],
+  htmltools[div],
+  shiny.fluent[Stack, Text],
 )
 
 #' @export
-make_card <- function(title, content, size = 12, style = "", is_contained = F) {
+make_card <- function(title, content, size = 12, style = "", is_contained = FALSE) {
   if (is_contained) {
-    card_class <- "card ms-depth-8" 
+    card_class <- "card ms-depth-8"
   } else {
     card_class <- glue("card ms-depth-8 ms-sm{size} ms-xl{size}")
   }
