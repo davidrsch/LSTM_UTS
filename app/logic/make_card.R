78b9bfdde5cf86f3fb0dc5a42873a80886d9a518
davidrsch <daviddrsch@gmail.com>
2024-09-05 23:09:08 +0200

    Modifying make a card function

--- app/logic/make_card.R
+++ app/logic/make_card.R
@@ -5,19 +5,28 @@ box::use(
 )
 
 #' @export
-make_card <- function(title, content, size = 12, style = "", is_contained = FALSE) {
+make_card <- function(title, content, size = 12, style = "", is_contained = FALSE, stacked = TRUE) {
   if (is_contained) {
     card_class <- "card ms-depth-8"
   } else {
     card_class <- glue("card ms-depth-8 ms-sm{size} ms-xl{size}")
   }
-  div(
-    class = card_class,
-    style = style,
-    Stack(
-      tokens = list(childrenGap = 10),
-      Text(variant = "xLarge", title, block = TRUE),
+  if (stacked) {
+    card <- div(
+      class = card_class,
+      style = style,
+      Stack(
+        tokens = list(childrenGap = 10),
+        Text(variant = "xLarge", title, block = TRUE),
+        content
+      )
+    )
+  } else {
+    card <- div(
+      class = card_class,
+      style = style,
       content
     )
-  )
+  }
+  return(card)  
 }
