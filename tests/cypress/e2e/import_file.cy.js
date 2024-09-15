1d860be9356143a156fd4a72171ac1f8cdc7e1d8
davidrsch <daviddrsch@gmail.com>
2024-09-16 01:22:06 +0200

    Add import_file tests

---
+++ tests/cypress/e2e/import_file.cy.js
@@ -0,0 +1,57 @@
+describe("Import file", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("'Import file' csv", () => {
+    cy.get("#app-import_file-file").should('be.visible');
+    cy.get('#app-import_file-file').attachFile('csv_example.csv');
+    cy.get('#app-table_output-data_table').wait(5000);
+    cy.fixture('data_table_text').then((expectedData) => {
+      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
+      });
+    });
+  });
+  
+  it("'Import file' tsv", () => {
+    cy.get("#app-import_file-file").should('be.visible');
+    cy.get('#app-import_file-file').attachFile('tsv_example.tsv');
+    cy.get('#app-table_output-data_table').wait(5000);
+    cy.fixture('data_table_tsv').then((expectedData) => {
+      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
+      });
+    });
+  });
+  
+  it("'Import file' xls", () => {
+    cy.get("#app-import_file-file").should('be.visible');
+    cy.get('#app-import_file-file').attachFile('xls_example.xls');
+    cy.get('#app-table_output-data_table').wait(5000);
+    cy.fixture('data_table_excel.json').then((expectedData) => {
+      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
+      });
+    });
+  });
+  
+  it("'Import file' xlsx", () => {
+    cy.get("#app-import_file-file").should('be.visible');
+    cy.get('#app-import_file-file').attachFile('xlsx_example.xlsx');
+    cy.get('#app-table_output-data_table').wait(5000);
+    cy.fixture('data_table_excel.json').then((expectedData) => {
+      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
+      });
+    });
+  });
+  
+  it("'Import file' incorrect extension", () => {
+    cy.get("#app-import_file-file").should('be.visible');
+    cy.get('#app-import_file-file').attachFile('LSTM_UTS-ss.png');
+    cy.get("#app-no_format_modal-make_modal-hideModal").should('be.visible');
+    cy.get("#app-no_format_modal-make_modal-hideModal").click();
+    cy.get("#app-no_format_modal-make_modal-hideModal").should('not.be.visible');
+  });
+  
+});
