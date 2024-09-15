6b8e7858e19fae40d21d62b6202af3fc265de35f
davidrsch <daviddrsch@gmail.com>
2024-09-15 03:18:34 +0200

    Finishing runmodal module

--- app/view/run_modal.R
+++ app/view/run_modal.R
@@ -1,17 +1,18 @@
 box::use(
-  dplyr[select],
+  dplyr[mutate, select],
   DT[dataTableOutput, renderDataTable],
-  shiny[div, moduleServer, NS, observeEvent, p, reactive, reactiveVal, tags],
   shiny.fluent[PrimaryButton.shinyInput],
-  stringr[str_split_i],
+  shiny[div, moduleServer, NS, observeEvent, p, reactive, reactiveVal, tags],
+  shinycssloaders[hidePageSpinner, showPageSpinner],
 )
 
 box::use(
   app/logic/determine_iterations[determine_iterations],
+  app/logic/process[process],
   app/view/make_modal,
 )
 
-# Error format modal ui
+# Run modal UI
 #' @export
 ui <- function(id) {
   ns <- NS(id)
@@ -19,8 +20,20 @@ ui <- function(id) {
   make_modal$ui(ns("make_modal"))
 }
 
+# The server of this module takes as additional inputs the data imported,
+# the sequence and forecast variables selected, the parameters selected
+# in training inputs (transformations, scales, horizon, inp_amount, lstm,
+# epoch, tests) and d_modal the visibility status of the download modal.
+# - Define condition to show the modal
+# - Store all possible combinations of training inputs in a data frame
+# - Create table of all possible combinations
+# - Create modal with table and start computation button
+# - Defining reactive value to store results and observe on start button
+# to do the computations, and display a spinner.
+# Return visibility reactive value and results.
 #' @export
-server <- function(id, transformations, scales, horizon, inp_amount, lstm, epoch, tests) {
+server <- function(id, data, sequence, forecast, transformations,
+  scales, horizon, inp_amount, lstm, epoch, tests, d_modal) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
@@ -46,9 +59,10 @@ server <- function(id, transformations, scales, horizon, inp_amount, lstm, epoch
       }
     })
 
+    # Creating table of all possible combinations
     output$iterations_table <- renderDataTable({
       if (all(iterations() != "")) {
-        iterations()  |> 
+        iterations()  |>
           select(-tests)
       }
     },
@@ -57,10 +71,9 @@ server <- function(id, transformations, scales, horizon, inp_amount, lstm, epoch
       lengthChange = FALSE,
       pageLength = 5,
       dom = "tip"
-    ),
-    server = TRUE)
-    
-    # Creating modla using make_modal module
+    ))
+
+    # Creating modal using make_modal module
     observeEvent(modal_visible(), {
       if (modal_visible()) {
         make_modal$server(
@@ -68,8 +81,9 @@ server <- function(id, transformations, scales, horizon, inp_amount, lstm, epoch
           is_open = modal_visible,
           title = "Warning",
           content = div(
-            p("You will excecute ", iterations()$tests[[1]], " tests of ", dim(iterations())[1], " models. ",
-           "Modify the previous form or filter if you whish to modify the models to test."),
+            p("You will execute ", iterations()$tests[[1]], " tests of ", dim(iterations())[1],
+            " models. Modify the previous form or filter if you whish to modify the",
+            " models to test."),
             tags$br(),
             dataTableOutput(ns("iterations_table")),
             tags$br(),
@@ -84,8 +98,34 @@ server <- function(id, transformations, scales, horizon, inp_amount, lstm, epoch
           status = "warning")
       }
     })
-    
-    reactive(modal_visible)
+
+    # Define reactive value to store results of computations and observer
+    # event to trigger computation
+    results <- reactiveVal("")
+    observeEvent(input$startbutton, {
+      showPageSpinner(
+        caption = "Please wait, this can take several minutes"
+      )
+      iterations_data <- iterations()[input$iterations_table_rows_all, ] |> 
+        mutate(
+          inp_amount = as.numeric(inp_amount),
+          lstm = as.numeric(lstm))
+      results(
+        process(
+          data,
+          sequence,
+          forecast,
+          iterations_data))
+      hidePageSpinner()
+      modal_visible(FALSE)
+      d_modal(TRUE)
+    })
+
+    reactive(
+      list(
+        modal_visible = modal_visible,
+        results = results
+      ))
 
   })
 }
