4dec318b2de356bca479e065e18fa91ef8ed425b
davidrsch <daviddrsch@gmail.com>
2024-09-26 09:28:47 +0200

    Adapting header test to use testid

--- tests/cypress/e2e/header.cy.js
+++ tests/cypress/e2e/header.cy.js
@@ -1,25 +1,15 @@
 describe("Header", () => {
   it("'Header' csv", () => {
     cy.visit("/");
-    cy.get("#app-import_file-file").should('be.visible');
-    cy.get('#app-import_file-upload_file')
+    cy.get('[data-testid="file"]').should('be.visible');
+    cy.get('[data-testid="upload_file"] [type="file"]')
       .should('not.be.visible')
       .selectFile('cypress/fixtures/csv_example.csv', {force: true});
-    cy.get('#app-table_output-data_table').wait(5000);
-    cy.fixture('data_table_text').then((expectedData) => {
-      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
-        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
-        if (currentData !== JSON.stringify(expectedData)) {
-          cy.log('DataTable data does not match the expected data');
-        } else {
-          cy.log('Importing csv files work as expected');
-        }
-      });
-    });
-    cy.get('#app-import_file-header').click({ force: true });
-    cy.get('#app-table_output-data_table').wait(5000);
+    cy.get('[data-testid="data_table"]').wait(5000);
+    cy.get('[data-testid="header"]').click({ force: true });
+    cy.get('[data-testid="data_table"]').wait(5000);
     cy.fixture('data_table_text_headless').then((expectedData) => {
-      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+      cy.getDataFromDatatable('data_table').then((currentData) => {
         expect(currentData).to.deep.equal(JSON.stringify(expectedData));
         if (currentData !== JSON.stringify(expectedData)) {
           cy.log('DataTable data does not match the expected data');
@@ -28,10 +18,10 @@ describe("Header", () => {
         }
       });
     });
-    cy.get('#app-import_file-header').click({ force: true });
-    cy.get('#app-table_output-data_table').wait(5000);
+    cy.get('[data-testid="header"]').click({ force: true });
+    cy.get('[data-testid="data_table"]').wait(5000);
     cy.fixture('data_table_text').then((expectedData) => {
-      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+      cy.getDataFromDatatable('data_table').then((currentData) => {
         expect(currentData).to.deep.equal(JSON.stringify(expectedData));
         if (currentData !== JSON.stringify(expectedData)) {
           cy.log('DataTable data does not match the expected data');
