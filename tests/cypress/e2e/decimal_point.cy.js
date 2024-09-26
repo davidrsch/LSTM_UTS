f4e67841c8ee3856be9194784ba71595ed89eaf1
davidrsch <daviddrsch@gmail.com>
2024-09-26 09:28:09 +0200

    Adapting decimal_point test to use testid

--- tests/cypress/e2e/decimal_point.cy.js
+++ tests/cypress/e2e/decimal_point.cy.js
@@ -5,16 +5,16 @@ describe("Decimal point", () => {
     cy.get('[data-testid="upload_file"] [type="file"]')
       .should('not.be.visible')
       .selectFile('cypress/fixtures/csv_example.csv', {force: true});
-    cy.get('#app-table_output-data_table').wait(5000);
+    cy.get('[data-testid="data_table"]').wait(5000);
     cy.fixture('data_table_text').then((expectedData) => {
-      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+      cy.getDataFromDatatable('data_table').then((currentData) => {
         expect(currentData).to.deep.equal(JSON.stringify(expectedData));
       });
     });
-    cy.get('#app-import_file-decimal_point').clear().type(',');
-    cy.get('#app-table_output-data_table').wait(5000);
+    cy.get('[data-testid="decimal_point"]').clear().type(',');
+    cy.get('[data-testid="data_table"]').wait(5000);
     cy.fixture('data_table_text_dp').then((expectedData) => {
-      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+      cy.getDataFromDatatable('data_table').then((currentData) => {
         expect(currentData).to.deep.equal(JSON.stringify(expectedData));
       });
     });
