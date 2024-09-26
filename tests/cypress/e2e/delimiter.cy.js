ded831cf7bfa3b766da2977f8bb41e4d7f8c8893
davidrsch <daviddrsch@gmail.com>
2024-09-26 09:28:33 +0200

    Adapting delimiter test to use testid

--- tests/cypress/e2e/delimiter.cy.js
+++ tests/cypress/e2e/delimiter.cy.js
@@ -1,20 +1,20 @@
 describe("Delimiter", () => {
   it("'Delimiter'", () => {
     cy.visit("/");
-    cy.get("#app-import_file-file").should('be.visible');
-    cy.get('#app-import_file-upload_file')
+    cy.get('[data-testid="file"]').should('be.visible');
+    cy.get('[data-testid="upload_file"] [type="file"]')
       .should('not.be.visible')
       .selectFile('cypress/fixtures/tsv_example.tsv', {force: true});
-    cy.get('#app-table_output-data_table').wait(5000);
+    cy.get('[data-testid="data_table"]').wait(5000);
     cy.fixture('data_table_tsv').then((expectedData) => {
-      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+      cy.getDataFromDatatable('data_table').then((currentData) => {
         expect(currentData).to.deep.equal(JSON.stringify(expectedData));
       });
     });
-    cy.get('#app-import_file-delimiter').clear().type('\t');
-    cy.get('#app-table_output-data_table').wait(5000);
+    cy.get('[data-testid="delimiter"]').clear().type('\t');
+    cy.get('[data-testid="data_table"]').wait(5000);
     cy.fixture('data_table_text').then((expectedData) => {
-      cy.getDataFromDatatable('app-table_output-data_table').then((currentData) => {
+      cy.getDataFromDatatable('data_table').then((currentData) => {
         expect(currentData).to.deep.equal(JSON.stringify(expectedData));
       });
     });
