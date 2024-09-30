ca7075f8abb97b68d30cb1533f311c15dc20f433
davidrsch <daviddrsch@gmail.com>
2024-09-30 09:26:59 +0200

    Improving test script

--- tests/cypress/e2e/delimiter.cy.js
+++ tests/cypress/e2e/delimiter.cy.js
@@ -1,22 +1,12 @@
 describe("Delimiter", () => {
-  it("'Delimiter'", () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.get('[data-testid="file"]').should('be.visible');
-    cy.get('[data-testid="upload_file"] [type="file"]')
-      .should('not.be.visible')
-      .selectFile('cypress/fixtures/tsv_example.tsv', {force: true});
-    cy.get('[data-testid="data_table"]').wait(5000);
-    cy.fixture('data_table_tsv').then((expectedData) => {
-      cy.getDataFromDatatable('data_table').then((currentData) => {
-        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
-      });
-    });
+    //Importing a data to test
+    cy.importing_data_flow("tsv");
+  });
+  it("'Delimiter'", () => {
     cy.get('[data-testid="delimiter"]').clear().type('\t');
     cy.get('[data-testid="data_table"]').wait(5000);
-    cy.fixture('data_table_text').then((expectedData) => {
-      cy.getDataFromDatatable('data_table').then((currentData) => {
-        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
-      });
-    });
+    cy.compare_table_fixture('data_table', 'data_table_text');
   });
 });
