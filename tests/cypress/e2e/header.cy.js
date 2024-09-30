3cb0326232a6a9ecb5e81f366469590cb431e840
davidrsch <daviddrsch@gmail.com>
2024-09-30 09:27:27 +0200

    Improving test script

--- tests/cypress/e2e/header.cy.js
+++ tests/cypress/e2e/header.cy.js
@@ -1,34 +1,15 @@
 describe("Header", () => {
-  it("'Header' csv", () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.get('[data-testid="file"]').should('be.visible');
-    cy.get('[data-testid="upload_file"] [type="file"]')
-      .should('not.be.visible')
-      .selectFile('cypress/fixtures/csv_example.csv', {force: true});
-    cy.get('[data-testid="data_table"]').wait(5000);
+    //Importing a data to test
+    cy.importing_data_flow("csv");
+  });
+  it("'Header'", () => {
     cy.get('[data-testid="header"]').click({ force: true });
     cy.get('[data-testid="data_table"]').wait(5000);
-    cy.fixture('data_table_text_headless').then((expectedData) => {
-      cy.getDataFromDatatable('data_table').then((currentData) => {
-        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
-        if (currentData !== JSON.stringify(expectedData)) {
-          cy.log('DataTable data does not match the expected data');
-        } else {
-          cy.log('Importing csv files work as expected');
-        }
-      });
-    });
+    cy.compare_table_fixture('data_table', 'data_table_text_headless');
     cy.get('[data-testid="header"]').click({ force: true });
     cy.get('[data-testid="data_table"]').wait(5000);
-    cy.fixture('data_table_text').then((expectedData) => {
-      cy.getDataFromDatatable('data_table').then((currentData) => {
-        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
-        if (currentData !== JSON.stringify(expectedData)) {
-          cy.log('DataTable data does not match the expected data');
-        } else {
-          cy.log('Importing csv files work as expected');
-        }
-      });
-    });
+    cy.compare_table_fixture('data_table', 'data_table_text');
   });
 })
