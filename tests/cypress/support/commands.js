eca92f4898fbfa498483c6289c3b822934289d9e
davidrsch <daviddrsch@gmail.com>
2024-09-30 09:25:54 +0200

    Adding compare table and import data flow commands

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -28,12 +28,28 @@ Cypress.Commands.add('getDataFromDatatable', (datatable_testid) => {
   });
 });
 
-Cypress.Commands.add('importing_data_flow', () => {
-  cy.get("#app-import_file-file").should('be.visible');
-  cy.get('#app-import_file-upload_file')
+Cypress.Commands.add('compare_table_fixture', (table, fixture) => {
+  cy.fixture(fixture).then((expectedData) => {
+    cy.getDataFromDatatable(table).then((currentData) => {
+      expect(currentData).to.deep.equal(JSON.stringify(expectedData));
+      if (currentData !== JSON.stringify(expectedData)) {
+        cy.log('DataTable data does not match the expected data');
+      } else {
+        cy.log('DataTable data match the expected data');
+      }
+    });
+  });
+})
+
+Cypress.Commands.add('importing_data_flow', (test_data) => {
+  cy.get('[data-testid="file"]').should('be.visible');
+  cy.get('[data-testid="upload_file"] [type="file"]')
       .should('not.be.visible')
-      .selectFile('cypress/fixtures/csv_example.csv', {force: true});
-  cy.get('#app-table_output-data_table').wait(5000);
+      .selectFile(`cypress/fixtures/${test_data}_example.${test_data}`, {force: true});
+  cy.get('[data-testid="data_table"]').wait(5000);
+});
+
+Cypress.Commands.add('select_variable_flow', () => {
   cy.get('#app-select_variables-sequence_variable').should('be.visible');
   cy.get('#app-select_variables-sequence_variable').click();
   cy.get('#app-select_variables-sequence_variable-list0').click();
