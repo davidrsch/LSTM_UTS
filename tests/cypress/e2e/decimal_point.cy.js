bff332d6f6a7de55661219e9e941968214725177
davidrsch <daviddrsch@gmail.com>
2024-09-30 09:26:39 +0200

    Improving test script

--- tests/cypress/e2e/decimal_point.cy.js
+++ tests/cypress/e2e/decimal_point.cy.js
@@ -1,22 +1,12 @@
 describe("Decimal point", () => {
-  it("'Decimal point'", () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.get('[data-testid="file"]').should('be.visible');
-    cy.get('[data-testid="upload_file"] [type="file"]')
-      .should('not.be.visible')
-      .selectFile('cypress/fixtures/csv_example.csv', {force: true});
-    cy.get('[data-testid="data_table"]').wait(5000);
-    cy.fixture('data_table_text').then((expectedData) => {
-      cy.getDataFromDatatable('data_table').then((currentData) => {
-        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
-      });
-    });
+    //Importing a data to test
+    cy.importing_data_flow("csv");
+  });
+  it("'Decimal point'", () => {
     cy.get('[data-testid="decimal_point"]').clear().type(',');
     cy.get('[data-testid="data_table"]').wait(5000);
-    cy.fixture('data_table_text_dp').then((expectedData) => {
-      cy.getDataFromDatatable('data_table').then((currentData) => {
-        expect(currentData).to.deep.equal(JSON.stringify(expectedData));
-      });
-    });
+    cy.compare_table_fixture('data_table', 'data_table_text_dp');
   });
 })
