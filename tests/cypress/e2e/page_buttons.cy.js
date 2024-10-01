ae408195babe3fd3a3438eb996a5ce3da8c080be
davidrsch <daviddrsch@gmail.com>
2024-10-01 08:08:44 +0200

    Improving page buttons test

--- tests/cypress/e2e/page_buttons.cy.js
+++ tests/cypress/e2e/page_buttons.cy.js
@@ -2,14 +2,34 @@ describe("Page buttons", () => {
     beforeEach(() => {
       cy.visit("/");
       //Importing a data to test
-      cy.importing_data_flow();
+      cy.importing_data_flow('csv');
     });
-    it("'Page buttons' turning pannel", () => {
-        cy.get('#app-page_buttons-nextbutton').should('be.visible');
-        cy.get('#app-page_buttons-nextbutton').click();
-        cy.get('#app-page_buttons-prevtbutton').should('be.visible');
-        cy.get('#app-page_buttons-prevtbutton').should('not.be.disabled');
-        cy.get('#app-page_buttons-nextbutton').should('be.visible');
-        cy.get('#app-page_buttons-nextbutton').should('be.disabled');
+    it("'Page buttons' visibility on forecast selection - showing", () => {
+      cy.select_flow('forecast_variable', '2');
+      cy.get('[data-testid="prevtbutton"]').should('be.visible');
+      cy.get('[data-testid="nextbutton"]').should('be.visible');
+    });
+    it("'Page buttons' visibility on forecast selection - hidding", () => {
+      cy.select_flow('forecast_variable', '2');
+      cy.select_flow('sequence_variable', '1');
+      cy.get('[data-testid="prevtbutton"]').should('not.be.visible');
+      cy.get('[data-testid="nextbutton"]').should('not.be.visible');
+    });
+
+    it("'Page buttons' turning", () => {
+      cy.select_flow('forecast_variable', '2');
+      cy.get('[data-testid="prevtbutton"]')
+        .should('be.visible')
+        .should('be.disabled');
+      cy.get('[data-testid="nextbutton"]')
+        .should('be.visible')
+        .should('not.be.disabled')
+        .click();
+      cy.get('[data-testid="prevtbutton"]')
+        .should('be.visible')
+        .should('not.be.disabled');
+      cy.get('[data-testid="nextbutton"]')
+        .should('be.visible')
+        .should('be.disabled');
     });
 });
