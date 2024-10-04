e04dbdd04cbfffb7e45bef7f9c76f37d0e423b5a
davidrsch <daviddrsch@gmail.com>
2024-10-04 08:45:32 +0200

    Adding tests for filters and start button

--- tests/cypress/e2e/run_modal.cy.js
+++ tests/cypress/e2e/run_modal.cy.js
@@ -24,4 +24,63 @@ describe("Run modal", () => {
       cy.compare_table_fixture('iterations_table', 'iterations');
     });
 
+    it("'Run modal' table filter visibility", () => {
+      cy.wait(5000);
+      cy.get('[data-testid="iterationsfilter-1"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-1', '1')
+        .should("have.text", "original");
+      cy.get_filter_option('iterationsfilter-1', '2')
+        .should("have.text", "first");
+      cy.get_filter_option('iterationsfilter-1', '3')
+        .should("have.text", "second");
+
+      cy.get('[data-testid="iterationsfilter-2"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-2', '1')
+        .should("have.text", "exact");
+      cy.get_filter_option('iterationsfilter-2', '2')
+        .should("have.text", "zero_one");
+      cy.get_filter_option('iterationsfilter-2', '3')
+        .should("have.text", "minus_plus");
+
+      cy.get('[data-testid="iterationsfilter-4"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-4', '1')
+        .should("have.text", "1");
+      cy.get_filter_option('iterationsfilter-4', '2')
+        .should("have.text", "2");
+
+      cy.get('[data-testid="iterationsfilter-5"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-5', '1')
+        .should("have.text", "16");
+      cy.get_filter_option('iterationsfilter-5', '2')
+        .should("have.text", "32");
+    });
+
+    it("'Run modal' table filter functionality", () => {
+      cy.wait(5000);
+      cy.get('[data-testid="iterationsfilter-1"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-1', '1')
+        .click({force: true});
+      cy.get('[data-testid="iterationsfilter-2"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-2', '1')
+        .click({force: true});
+      cy.get_filter_option('iterationsfilter-2', '2')
+        .click({force: true});
+      cy.get('[data-testid="iterationsfilter-4"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-4', '1')
+        .click({force: true});
+      cy.get('[data-testid="iterationsfilter-5"]').click({force: true});
+      cy.get_filter_option('iterationsfilter-5', '1')
+        .click({force: true});
+      cy.compare_table_fixture('iterations_table', 'iterations_fil');
+    });
+
+    it("'Run modal' start button", () => {
+      cy.wait(5000);
+      cy.get('[data-testid="startbutton"]').click({force: true});
+      cy.get('.loader')
+        .should('be.visible');
+      cy.get('.shiny-spinner-caption')
+        .should('have.text', 'Please wait, this can take several minutes');
+    });
+
 });
