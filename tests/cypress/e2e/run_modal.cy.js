ce15d26f4dad2d1b5bc73ba6dabf9b1fbb5610a9
davidrsch <daviddrsch@gmail.com>
2024-10-04 08:41:42 +0200

    Adding run modal visibility test

---
+++ tests/cypress/e2e/run_modal.cy.js
@@ -0,0 +1,22 @@
+describe("Run modal", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      //Importing a data to test
+      cy.importing_data_flow('csv');
+      //Select variables
+      cy.select_flow('sequence_variable', '1');
+      cy.select_flow('forecast_variable', '2');
+      //Turn panel
+      cy.turn_panel();
+      cy.trainin_test_inputs_flow();
+      cy.run_flow();
+    });
+
+    it("'Run modal' visibility", () => {
+      cy.get('[data-testid="close_run_modal"]')
+      .should('be.visible')
+      .click()
+      .should('not.be.visible');
+    });
+
+});
