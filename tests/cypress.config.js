4c859bef0b52dd9d5e134a3fd5b6939678f974de
davidrsch <daviddrsch@gmail.com>
2024-10-11 17:02:00 +0200

    Adding variable storing downloads folder path

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,5 +3,6 @@ module.exports = {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3333',
     supportFile: 'cypress/support/commands.js',
+    downloadsFolder: 'cypress/downloads',
   },
 }
